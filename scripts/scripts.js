3a6175d200b04cf6101f546b6527df8d1a451b24
Trukken <tomolya12@gmail.com>
2019-10-01 14:01:13 +0200

    finished chat functions login and signin

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,25 @@
-
-getMessage();
-
-setInterval(function () {
+const isIt = window.location.href.indexOf("chat-room.html") ? true : false;
+console.log(isIt)
+if (isIt) {
     getMessage();
-    console.log("Time and time again");
-}, 10000);
 
+    let chatRoomLogin = 0;
+
+    setInterval(function () {
+        getMessage();
+        console.log("Time and time again");
+    }, 10000);
+}
 function getMessage() {
     let xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             let message = JSON.parse(xhttp.responseText).messagesArray;
-            loadMessage(message);
+            try {
+                loadMessage(message);
+            } catch (err) {
+
+            }
         }
     }
     xhttp.open('GET', 'https://ajax.igor-marty.fr/messages', true)
@@ -36,13 +44,11 @@ try {
     const submitMessage = document.querySelector(".submitMessage")
     submitMessage.addEventListener("submit", postMessage);
 } catch (err) {
-    console.log(err);
 }
 
 function postMessage() {
     event.preventDefault();
     const textArea = document.querySelector("#textArea").value;
-    textArea.innerText = "";
     fetch('https://ajax.igor-marty.fr/messages', {
         method: 'POST',
         headers: {
@@ -57,6 +63,7 @@ function postMessage() {
         .then((data) => {
             console.log(data);
             getMessage();
+            document.getElementById('textArea').value = '';
         })
         .catch((err) => console.log(err));
     console.log(textArea);
@@ -85,7 +92,6 @@ try {
     const signUp = document.querySelector(".signUp");
     signUp.addEventListener("submit", singUpNow)
 } catch (err) {
-    console.log(err);
 }
 function singUpNow() {
     event.preventDefault();
@@ -106,9 +112,38 @@ function singUpNow() {
         .then((data) => {
             console.log(data);
             localStorage.setItem("chatRoomLogin", data.code);
+            window.location.href = "./chat-room.html";
         })
         .catch((err) => console.log(err));
 }
 
+try {
+    const login = document.querySelector(".login")
+    login.addEventListener("submit", logIn)
+} catch (err) {
 
+}
 
+function logIn() {
+    event.preventDefault();
+    const userName = document.querySelector("#userLoginID").value;
+    const passWord = document.querySelector("#userLoginPW").value;
+
+    fetch('https://ajax.igor-marty.fr/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            login: userName,
+            password: passWord
+        })
+    }).then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            localStorage.setItem("chatRoomLogin", data.code);
+            window.location.href = "./chat-room.html";
+        })
+        .catch((err) => console.log(err));
+}
